04c62e42c32ad75a08f1632c73b9091c76979626
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2024-10-27 18:17:25 -0400

    adding add_interest_loop func

--- bank_account.py
+++ bank_account.py
@@ -6,13 +6,21 @@ import string
 class BankAccount:
 
     # Create all of the following below
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number, balance, type):
         # define the name of the owner of the accounr
         self.full_name = full_name
         # define the unique account number of the owner
         self.account_number = account_number if account_number else ''.join(random.choices(string.digits, k=8))
         # define the balance of money in the account, it should start with 0
         self.balance = balance
+        # type of account
+        self.type = type
+
+        # if type is equal to savings then add interest, else do nothing
+        if self.type == "savings":
+            self.balance += balance * 0.01
+        else:
+            pass
 
     # add money to the account
     def deposit(self, amount):
@@ -27,6 +35,7 @@ class BankAccount:
         self.balance -= amount
         # if amount is more than balance say you have insufficient funds and charge $10 as overdraft fee
         if amount > self.balance:
+            print("Insufficient funds.")
             self.balance -= 10
             # say new balance
             print(f"New balance: ${self.balance:,.2f}")
@@ -50,22 +59,27 @@ class BankAccount:
     def print_statement(self):
         # say name, acount number sensitized, routing number, balance in currency format
         routing_number = 12341234
-        account_number_sensitized = "*" * 4 + str(self.account_number)[3:]
+        account_number_sensitized = "*" * 4 + str(self.account_number)[4:]
         print(f"""{self.full_name}\nAccount No: {account_number_sensitized}
 Routing No: {routing_number}\nBalance: ${self.balance:,.2f}""")
 
 # create first example acconout
-marhia_account = BankAccount("Marhia Johns", 0, 1000)
+marhia_account = BankAccount("Marhia Johns", 0, 1000, "savings")
+# marhia_account.print_statement()
+# print(marhia_account.account_number)
 
 # create second example acconout
-hans_account = BankAccount("Hans Lahm", 0, 500)
+hans_account = BankAccount("Hans Lahm", 0, 500, "checking")
+# hans_account.print_statement()
+# print(hans_account.account_number)
 
 # create third example acconout 
-Sarah_account = BankAccount("Sarah Abdul", 0, 5000)
-
+Sarah_account = BankAccount("Sarah Abdul", 0, 5000, "savings")
+# Sarah_account.print_statement()
+# print(Sarah_account.account_number)
 
 # create another account. set his name to Mitchell, and his account number to 03141592
-mitchell_account = BankAccount("Mitchell Hudson", 3141592, 0 )
+mitchell_account = BankAccount("Mitchell Hudson", f"{3141592:08}", 0, "checking" )
 
 # deposit $400,000 to Mitchell account 
 mitchell_account.deposit(400000)
@@ -83,4 +97,17 @@ mitchell_account.print_statement()
 mitchell_account.withdraw(150)
 
 # say his statement
+mitchell_account.print_statement()
+
+# bank list
+bank = [mitchell_account, Sarah_account, hans_account, marhia_account]
+
+# go over all accounts in bank and add interest to each.
+def add_interst_loop(bank):
+    for account in bank:
+        account.add_interest()
+
+# executing code adding interest to each account
+add_interst_loop(bank)
+
 mitchell_account.print_statement()
